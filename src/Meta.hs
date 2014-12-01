ea46ad751bf17341154d0162e1d0644b736e88dd
aaleksander <aleksandranufriev@yandex.ru>
2014-12-01 23:10:40 +0400

    начал Мета-траектории

---
+++ src/Meta.hs
@@ -0,0 +1,28 @@
+module Meta where
+--мета - траектории
+import Test.HUnit
+import TestExt
+import GCode hiding (main)
+import Export hiding (main)
+import Utils hiding (main)
+
+data MPoint = 
+		MPnt Double Double  			-- обычная точка (угол)
+	| 	MRond Double Double Double   	-- скругленный угол
+	|	MCircle Double Double Double	-- окружность с радиусом
+	deriving (Show, Eq)
+
+--преобразует траекторию в g-код
+meta2GCode :: [MPoint] -> Direction -> Double -> CNC
+meta2GCode l dir safeZ = undefined
+
+tests = [
+	assertCNC "test_01"
+			  (meta2GCode [MPnt 0 0, MPnt 10 0, MPnt 10 10, MPnt 0 10] CW 5)
+			  "dd"
+	]
+
+
+
+main = 
+	testing Meta.tests
